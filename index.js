47af75f1c080c1b6c2928b14454eb0fae8133900
rohith-s-s <rohithss2001@gmail.com>
2025-04-27 20:32:17 +0530

    DNS Check enabled

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+const { hostname } = require("os");
 
 // Basic Configuration
 app.use(express.json());
@@ -10,37 +12,37 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
 let shortUrl;
 let url;
 
-app.post("/api/shorturl", (req,res)=>{
-  url = req.body.url;
-  try {
-  new URL(url);
-}
-  catch (err) {
-    return res.json({ "error": 'invalid url' })
-  }
-  shortUrl = Math.floor(Math.random() * 10);
-  res.json({"original_url":url,"short_url":shortUrl})
-})
 
-app.get("/api/shorturl/:shortUrlId",(req,res)=>{
+app.post("/api/shorturl",(req, res) => {
+  url = req.body.url;
+  const hostname = new URL(url).hostname
+  dns.lookup(hostname, (err)=>{
+    if(err) return res.json({"error":"invalid url"})
+      shortUrl = Math.floor(Math.random() * 10);
+      res.json({ original_url: url, short_url: shortUrl });
+  } )
+  
+});
+
+app.get("/api/shorturl/:shortUrlId", (req, res) => {
   const shortUrlId = parseInt(req.params.shortUrlId);
-  if(shortUrl === shortUrlId ) res.redirect(url)
-    res.json({"error": 'invalid url'})
-})
-app.listen(port, function() {
+  if (shortUrl === shortUrlId) res.redirect(url);
+  res.json({ error: "invalid url" });
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
